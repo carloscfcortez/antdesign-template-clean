c77a89bfd2453f1d2f383724ec6b7c1d17cbc7e0
Carlos Cortez <carlos.cfcortez@gmail.com>
2019-03-18 11:39:01 -0400

    first commit

---
+++ src/components/SiderMenu/index.js
@@ -0,0 +1,112 @@
+import React from 'react'
+
+import { Layout, Menu, Breadcrumb, Icon } from 'antd'
+
+import logo from './../../logo.svg'
+
+import './style.css'
+
+const { Header, Content, Footer, Sider } = Layout
+const SubMenu = Menu.SubMenu
+
+export default class SiderMenu extends React.Component {
+  state = {
+    collapsed: false
+  }
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    })
+  }
+
+  onCollapse = collapsed => {
+    console.log(collapsed)
+    this.setState({ collapsed })
+  }
+
+  render () {
+    return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
+          // trigger={null}
+        >
+          <div className='logo'>
+            <img src={logo} alt='Logo' />
+          </div>
+          <Menu theme='dark' defaultSelectedKeys={['1']} mode='inline'>
+            <Menu.Item key='1'>
+              <Icon type='pie-chart' />
+              <span>Option 1</span>
+            </Menu.Item>
+            <Menu.Item key='2'>
+              <Icon type='desktop' />
+              <span>Option 2</span>
+            </Menu.Item>
+            <SubMenu
+              key='sub1'
+              title={
+                <span>
+                  <Icon type='user' />
+                  <span>User</span>
+                </span>
+              }
+            >
+              <Menu.Item key='3'>Tom</Menu.Item>
+              <Menu.Item key='4'>Bill</Menu.Item>
+              <Menu.Item key='5'>Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key='sub2'
+              title={
+                <span>
+                  <Icon type='team' />
+                  <span>Team</span>
+                </span>
+              }
+            >
+              <Menu.Item key='6'>Team 1</Menu.Item>
+              <Menu.Item key='8'>Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key='9'>
+              <Icon type='file' />
+              <span>File</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', paddingLeft: '15px' }}>
+            <Icon
+              className='trigger'
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+          </Header>
+          <Content
+            style={{
+              margin: '4px 6px',
+              padding: 24,
+              background: '#fff',
+              minHeight: 280
+            }}
+          >
+            <Breadcrumb style={{ margin: '6px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
+    )
+  }
+}
+
+// ReactDOM.render(<SiderDemo />, mountNode)
